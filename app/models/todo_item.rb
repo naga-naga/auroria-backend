d826a238a09539560fd649ee0cf0dfb113461b3d
naga-naga <kiyoharu.639721@gmail.com>
2025-08-15 01:39:42 +0900

    TodoItem の status を enum にした

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class TodoItem < ApplicationRecord
+  enum :status, {
+    incomplete: 'incomplete',
+    done: 'done',
+  }
 end
--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -3,8 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe TodoItem do
-  it 'モデルが valid であること' do
-    todo_item = described_class.build(detail: '歯を磨く', status: :done)
-    expect(todo_item).to be_valid
+  describe 'status について' do
+    it 'status が incomplete の場合 valid' do
+      todo_item = described_class.build(detail: 'foo', status: :incomplete)
+      expect(todo_item).to be_valid
+    end
+
+    it 'status が done の場合 valid' do
+      todo_item = described_class.build(detail: 'foo', status: :done)
+      expect(todo_item).to be_valid
+    end
+
+    it 'status が上記以外の場合エラー' do
+      expect { described_class.build(detail: 'foo', status: :foo) }.to raise_error(ArgumentError)
+    end
   end
 end
