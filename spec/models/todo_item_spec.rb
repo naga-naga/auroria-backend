dcfc9e9327f9089a95aaf3761ce81ccceab3b979
naga-naga <kiyoharu.639721@gmail.com>
2025-08-16 01:49:30 +0900

    RSpec/DescribedClass を explicit にした
    クラス名は明示的に書かれていてほしいため

--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -8,23 +8,23 @@ RSpec.describe TodoItem do
       todo_list = TodoList.create!(title: 'title')
       todo_item = todo_list.todo_items.create!(detail: 'foo', status: :incomplete)
 
-      expect(described_class.find(todo_item.id).todo_list).to eq(TodoList.find(todo_list.id))
+      expect(TodoItem.find(todo_item.id).todo_list).to eq(TodoList.find(todo_list.id))
     end
   end
 
   describe 'status について' do
     it 'status が incomplete の場合 valid' do
-      todo_item = described_class.build(detail: 'foo', status: :incomplete)
+      todo_item = TodoItem.build(detail: 'foo', status: :incomplete)
       expect(todo_item).to be_valid
     end
 
     it 'status が done の場合 valid' do
-      todo_item = described_class.build(detail: 'foo', status: :done)
+      todo_item = TodoItem.build(detail: 'foo', status: :done)
       expect(todo_item).to be_valid
     end
 
     it 'status が上記以外の場合エラー' do
-      expect { described_class.build(detail: 'foo', status: :foo) }.to raise_error(ArgumentError)
+      expect { TodoItem.build(detail: 'foo', status: :foo) }.to raise_error(ArgumentError)
     end
   end
 end
--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -5,14 +5,14 @@ require 'rails_helper'
 RSpec.describe TodoList do
   describe 'todo_items の関連付けについて' do
     it 'TodoList から辿れる' do
-      todo_list = described_class.create!(title: 'title')
+      todo_list = TodoList.create!(title: 'title')
       todo_item = todo_list.todo_items.create!(detail: 'foo', status: :incomplete)
 
-      expect(described_class.find(todo_list.id).todo_items).to contain_exactly(TodoItem.find(todo_item.id))
+      expect(TodoList.find(todo_list.id).todo_items).to contain_exactly(TodoItem.find(todo_item.id))
     end
 
     it 'TodoList を消すと TodoItem も消える' do
-      todo_list = described_class.create!(title: 'title')
+      todo_list = TodoList.create!(title: 'title')
       todo_list.todo_items.create!(detail: 'foo', status: :incomplete)
 
       expect { todo_list.destroy! }.to change(TodoItem, :count).by(-1)
@@ -21,17 +21,17 @@ RSpec.describe TodoList do
 
   describe 'title について' do
     it '空文字列でない文字列の場合 valid' do
-      todo_list = described_class.build(title: 'foo')
+      todo_list = TodoList.build(title: 'foo')
       expect(todo_list).to be_valid
     end
 
     it '空文字列の場合 valid' do
-      todo_list = described_class.build(title: '')
+      todo_list = TodoList.build(title: '')
       expect(todo_list).to be_valid
     end
 
     it 'nil の場合 invalid' do
-      todo_list = described_class.build(title: nil)
+      todo_list = TodoList.build(title: nil)
       expect(todo_list).not_to be_valid
     end
   end
