a5059c0a84584dd462e2e80fb9496e2e19a2f116
naga-naga <kiyoharu.639721@gmail.com>
2025-08-25 01:37:28 +0900

    GET /api/v1/todo_lists/:id を作成

--- app/controllers/api/v1/todo_lists_controller.rb
+++ app/controllers/api/v1/todo_lists_controller.rb
@@ -3,7 +3,14 @@
 module Api
   module V1
     class TodoListsController < ApplicationController
-      def show; end
+      def show
+        todo_list = TodoList.find(params[:id])
+
+        render json: {
+          id: todo_list.id,
+          title: todo_list.title,
+        }
+      end
     end
   end
 end
